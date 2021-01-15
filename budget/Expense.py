51e2362862af55dcb5e02ca97de4b4ed14fd2ffa
ubuntu <ubuntu@ubuntu.man>
2021-01-14 22:20:32 -0500

    module4 update

--- budget/Expense.py
+++ budget/Expense.py
@@ -43,7 +43,7 @@ class Expenses():
 
     def categorize_set_comprehension(self):
         necessary_expenses = {x for x in self.list}
-        if(x.category == 'Phone' or x.category == 'Auto and Gas') or x.category == 'Classes' or x.category == 'Utilities' or x.category == 'Mortgage'):
-            food_expenses = {x for x in self.list if x.category == 'Groceries' or if x.category = 'Eating Out'}
+        #if(x.category == 'Phone' or x.category == 'Auto and Gas' or x.category == 'Classes' or x.category == 'Utilities' or x.category == 'Mortgage'):
+        food_expenses = {x for x in self.list if x.category == 'Groceries' or x.category == 'Eating Out'}
         unnecessary_expenses = set(self.list) - necessary_expenses - food_expenses
         return [necessary_expenses, food_expenses, unnecessary_expenses]
