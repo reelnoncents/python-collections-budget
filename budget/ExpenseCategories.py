84edc518efc6914a29162e758fdef511336a6178
ubuntu <ubuntu@ubuntu.man>
2021-01-14 22:50:30 -0500

    mod4 update3

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -12,7 +12,7 @@ def main():
 
     for a, b in zip(divided_for_loop, divided_set_comp):
         if not (a.issubset(b) and b.issubset(a)):
-            print("Set are NOT equal by subset test")
+            print("Sets are NOT equal by subset test")
 
 
 if __name__ == "__main__":
